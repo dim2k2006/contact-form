c93d44b951f2da1506a9dfd803fd05c3499cab68
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-02-25 23:52:47 +0300

    Add "Root" component

---
+++ src/components/Root.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import PropTypes from 'prop-types';
+import Form from './Form';
+
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <section className="Root p-4">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-sm-6">
+            <Form />
+          </div>
+        </div>
+      </div>
+    </section>
+  </Provider>
+);
+
+Root.propTypes = {
+  store: PropTypes.shape({
+    subscribe: PropTypes.func.isRequired,
+    dispatch: PropTypes.func.isRequired,
+    getState: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default Root;
