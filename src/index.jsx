16b32b4631df8f34f4297d9592a7eab2640f8d76
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-02-25 23:47:51 +0300

    Remove unneeded files

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import 'normalize.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
+import reducer from './redux/slices';
+import Root from './components/Root';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore({
+  reducer,
+});
+
+ReactDOM.render(<Root store={store} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
